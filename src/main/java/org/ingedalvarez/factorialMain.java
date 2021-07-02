2b0fa27430a53ec4d2ffc2f89489d622c2adc24b
ingedalvarez <ingedalvarez@gmail.com>
2021-07-02 16:34:02 -0500

    Implementacion de la clase factorialMain donde se prueban varias formas de solucionar la funcion recursiva de n factorial.

---
+++ src/main/java/org/ingedalvarez/factorialMain.java
@@ -0,0 +1,57 @@
+package org.ingedalvarez;
+
+
+import org.ingedalvarez.ch4.AStack;
+import org.ingedalvarez.ch4.Stack;
+
+public class factorialMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Some exercises." );
+
+        System.out.println(factRecu(7));
+        System.out.println(factBucle(7));
+        System.out.println(factAStack(7));
+    }
+
+    static long factRecu(int n){
+        assert (n >= 0) && (n <= 20) : "n out of range";
+        if(n == 0) {
+            System.out.println("Llamada a factRecu de : "+ n);
+            return 1;
+        }
+        else{
+            System.out.println("Llamada a factRecu de : "+ n);
+            return n * factRecu(n-1);
+        }
+    }
+
+    static long factBucle(int n){
+        assert (n >= 0) && (n <= 20) : "n out of range";
+        long fact = 1;
+        for(int i = 1; i <= n; i++){
+            fact = fact * (i);
+            System.out.println("Iteracion numero "+ i);
+        }
+        return fact;
+    }
+
+    static long factAStack(int n){
+        // To fit n! in a long variable, require n < 21
+        assert (n >= 0) && (n <= 20) : "n out of range";
+        // Make a stack just big enough
+        Stack<Integer> S = new AStack<Integer>(n);
+        while (n > 1) {
+            System.out.println("llamada al primer while ");
+            S.push(n--);
+        }
+        long result = 1;
+        while (S.length() > 0) {
+            System.out.println("llamada al segundo while ");
+            result = result * S.pop();
+        }
+        return result;
+    }
+
+}
