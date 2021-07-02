e9bb9e39cc174314cdc2626f2109737cf6c7585b
ingedalvarez <ingedalvarez@gmail.com>
2021-07-02 11:51:33 -0500

    implementation of class LStack

---
+++ src/main/java/org/ingedalvarez/ch4/LStack.java
@@ -0,0 +1,41 @@
+package org.ingedalvarez.ch4;
+
+public class LStack<E> implements Stack<E> {
+    private Link<E> top;    // Pointer to first element
+    private int size;    // Number of elements
+
+    //Constructors
+    public LStack() {
+        top = null;
+        size = 0;
+    }
+
+    public LStack(int size) {
+        top = null;
+        size = 0;
+    }
+
+    public void clear() {
+        top = null;    } // Reinitialize stack
+
+    public void push(E it) {    // Put "it" on stack
+        top = new Link<E>(it, top);
+        size++;
+    }
+
+    public E pop() {    // Remove "it" from stack
+        assert top != null : "Stack is empty";
+        E it = top.element();
+        top = top.next();
+        size--;
+        return it;
+    }
+
+    public E topValue() {   // Return top value
+        assert top != null : "Stack is empty";
+        return top.element();
+    }
+
+    public int length() {
+        return size; }  // Return length
+}
